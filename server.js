0d2d4f1d58b1bdc42fe8cef48209306932c057c7
Thitithorn <Thitithorn.su@gmail.com>
2024-12-09 11:51:07 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,147 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
+const User = require("./models/user.model");
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+
+
+app.get("/", (req, res) => {
+  const abc = process.env.ACCESS_TOKEN_SECRET  
+  .status(400)
+  res.json({ data: "hello" });
+});
+
+//Create Account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  const abc = process.env.ACCESS_TOKEN_SECRET
+  
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "มีผู้ใช้งานนี้แล้ว",
+    });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "3600m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration Successful",
+  });
+});
+
+// Login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "กรุณาตรวจสอบEmail" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ massage: "ไม่พบผู้ใช้" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "3600m",
+    });
+
+    return res.json({
+      error: false,
+      message: "Login สำเร็จ!",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid Credentials",
+    });
+  }
+});
+
+// Get user
+app.get("/get-user", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  const isUser = await User.findOne({ _id: user._id });
+
+  if (!isUser) {
+    return res.status(401);
+  }
+
+  return res.json({
+    user: {
+      fullName: isUser.fullName,
+      email: isUser.email,
+      _id: isUser._id,
+      createOn: isUser.createOn,
+    },
+    message: "",
+  });
+});
+
+
+app.listen(8000);
+
+module.exports = app;
