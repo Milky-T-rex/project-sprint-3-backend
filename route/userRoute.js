1f314765d44c362dc8d428a822eaa2003210667d
Thitithorn <Thitithorn.su@gmail.com>
2024-12-16 10:09:19 +0700

    Uptodate

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import { loginUser, registerUser, adminLogin } from "../controllers/userController.js";
 // import { authenticateToken } from "../middleware/utilities.js"
 
 const userRouter = express.Router();
 
 userRouter.post("/login", loginUser);
 userRouter.post("/register", registerUser);
+userRouter.post("/admin", adminLogin);
 // userRouter.post("/register",authenticateToken, registerUser);
 
 export default userRouter;
